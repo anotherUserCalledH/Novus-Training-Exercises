1b3aa4dff1ecf1abd047bd4c8f6ae92773509448
Henry <henryb1033@gmail.com>
2024-01-05 14:29:59 +0000

    Update Front11

--- CodingBatArray1/Front11/Program.cs
+++ CodingBatArray1/Front11/Program.cs
@@ -4,14 +4,20 @@
 	{
 		public static int[] Front11(int[] a, int[] b)
 		{
-			int[] output = new int[2];
+			int[] output;
 
-			if(a.Length > 0)
+			if(a.Length < 1)
 			{
-				output[0] = a[0];
+				output = new int[] { b[0]};
 			}
-			if(b.Length > 0)
+			else if(b.Length < 1)
 			{
+				output = new int[] { a[0] };
+			}
+			else
+			{
+				output = new int[2];
+				output[0] = a[0];
 				output[1] = b[0];
 			}
 
@@ -26,7 +32,7 @@
 		{
 			Console.WriteLine(BuildOutput(new int[] { 1, 2, 3 }, new int[] { 7, 9, 8 }));
 			Console.WriteLine(BuildOutput(new int[] { 1 }, new int[] { 2 }));
-			Console.WriteLine(BuildOutput(new int[] { 2, 7 }, new int[] { }));
+			Console.WriteLine(BuildOutput(new int[] { 1, 7 }, new int[] { }));
 
 		}
 	}
